95ccb965bdda3338c975f42446410a3282997196
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2022-11-10 09:12:54 +0100

    wasm

---
+++ public/draw.mjs
@@ -0,0 +1,37 @@
+import { getMathCoords } from './mathCoords.mjs';
+import { renderMandelbrot } from './build/debug.js';
+
+let pending = 0;
+
+export async function draw(canvas) {
+    if (++pending > 1) {
+        return;
+    }
+
+    do {
+        const acquired = pending;
+        await draw0(canvas);
+        pending = Math.max(pending - acquired, 0);
+    } while (pending);
+}
+
+async function draw0(canvas) {
+    const coords = getMathCoords(canvas);
+    const rgbaArray = renderMandelbrot(
+        Number(coords.unit),
+        Number(coords.xMin),
+        Number(coords.w),
+        Number(coords.yMin),
+        Number(coords.h),
+        canvas.width,
+        canvas.height,
+    );
+    // const response = await fetch(`/api/draw?unit=${coords.unit}&xMin=${coords.xMin}&w=${coords.w}&yMin=${coords.yMin}&h=${coords.h}&canvasW=${canvas.width}&canvasH=${canvas.height}`);
+    // const rgbaArray = new Uint8ClampedArray(await response.arrayBuffer())
+    const ctx = canvas.getContext('2d')
+    ctx.putImageData(
+        new ImageData(rgbaArray, canvas.width, canvas.height),
+        0,
+        0
+    )
+}
