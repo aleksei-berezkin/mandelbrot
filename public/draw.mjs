200998e570e2cefc21d18adc40a515e8c775729d
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2022-11-15 20:35:59 +0100

    SplitWip

--- public/draw.mjs
+++ public/draw.mjs
@@ -9,7 +9,7 @@ export async function draw(canvas) {
 
     do {
         const acquired = pending;
-        await sendToWorker(canvas);
+        await draw0(canvas);
         pending = Math.max(pending - acquired, 0);
     } while (pending);
 }
@@ -19,7 +19,7 @@ const drawWorker = new Worker('drawWorker.js');
 /**
  * @param canvas {HTMLCanvasElement}
  */
-async function sendToWorker(canvas) {
+async function draw0(canvas) {
     const coords = getMathCoords(canvas);
     drawWorker.postMessage([coords, canvas.width, canvas.height]);
 
@@ -33,4 +33,53 @@ async function sendToWorker(canvas) {
             resolve();
         }
     })
+}
+
+/**
+ * @param worker {Worker}
+ * @param coords {Coords}
+ * @param canvasW {number}
+ * @param canvasH {number}
+ */
+async function renderOnWorker(worker, coords, canvasW, canvasH) {
+
+}
+
+/**
+ * @param yMin {BigInt}
+ * @param h {BigInt}
+ * @param canvasH {number}
+ * @param n {number}
+ * @return {Generator<{yMin: BigInt, h: BigInt, canvasYMin: number, canvasH: number}>}
+ */
+function* splitWork(yMin, h, canvasH, n) {
+    if (h < 32) {
+        yield {yMin, h, canvasYMin: 0, canvasH};
+        // noinspection JSValidateTypes
+        return;
+    }
+
+    const yPart = h / BigInt(n);
+    const canvasHPart = Math.round(canvasH / n);
+
+    let yDone = 0n;
+    let canvasYDone = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        yield {
+            yMin: yMin + yDone,
+            h: yPart,
+            canvasYMin: canvasYDone,
+            canvasH: canvasHPart,
+        };
+        yDone += yPart;
+        canvasYDone += canvasHPart;
+    }
+
+    yield {
+        yMin: yMin + yDone,
+        h: h - yDone,
+        canvasYMin: canvasYDone,
+        canvasH: canvasH - canvasYDone,
+    };
 }
