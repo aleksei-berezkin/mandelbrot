65195af9cf7f98465b0f2023368a56f959c44067
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2022-12-23 15:46:28 +0100

    Touches

--- public/trackTouch.mjs
+++ public/trackTouch.mjs
@@ -1,25 +1,85 @@
 import { isGesture } from './isGesture.mjs';
+import { getMathCoords, moveCoords, setMathCoords, zoomCoords } from './mathCoords.mjs';
+import { render } from './render.mjs';
 
 /**
  * @param canvas {HTMLCanvasElement}
  */
 export function trackTouch(canvas) {
 
+    /**
+     * @type {TouchList}
+     */
+    let prevTouches = undefined;
+
     canvas.addEventListener('touchstart', e => {
-        console.log('touchstart', e.touches);
         if (!isGesture(e)) return;
 
         e.preventDefault();
+
+        prevTouches = e.touches;
     }, {passive: false});
 
     canvas.addEventListener('touchmove', e => {
         console.log('touchmove', e.touches);
-        if (!isGesture(e)) return;
+        if (!isGesture(e) || !prevTouches) return;
+
+        const {width, height} = canvas.getBoundingClientRect();
+
+        const prevCenter = getCenter(prevTouches);
+        const newCenter = getCenter(e.touches);
+        const newCenterFr = {
+            x: newCenter.x / width,
+            y: 1 - newCenter.y / height,
+        };
+        const moveDeltaFr = {
+            x: (newCenter.x - prevCenter.x) / width,
+            y: 1 - (newCenter.y - prevCenter.y) / height,
+        };
+        const prevPinchSize = getPinchSize(prevTouches);
+        const newPinchSize = getPinchSize(e.touches);
+
+        const coords = getMathCoords(canvas);
+        const movedCoords = moveCoords(coords, moveDeltaFr);
+        const zoomedCoords = zoomCoords(movedCoords, newCenterFr, newPinchSize / prevPinchSize);
+
+        setMathCoords(canvas, zoomedCoords);
+
+        prevTouches = e.touches;
+
+        setTimeout(() => render(canvas), 100);
 
     }, {passive: false});
 
     canvas.addEventListener('touchend', e => {
-        if (!isGesture(e)) return;
+        if (!isGesture(e) || !prevTouches) return;
+
+        prevTouches = undefined;
 
     }, {passive: false});
+}
+
+/**
+ * @param touches {TouchList}
+ * @return {{x: number, y: number}}
+ */
+function getCenter(touches) {
+    return [...touches].reduce(
+        (avg, touch) => ({
+            x: avg.x + touch.clientX / touches.length,
+            y: avg.y + touch.clientY / touches.length,
+        }),
+        {x: 0, y: 0},
+    );
+}
+
+/**
+ * @param touches {TouchList}
+ * @return {number}
+ */
+function getPinchSize(touches) {
+    return Math.hypot(
+        touches[1].clientX - touches[0].clientX,
+        touches[1].clientY - touches[0].clientY,
+    )
 }
