82de214669dc280294bf58261efac358f28bb4bd
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2023-01-20 23:28:04 +0100

    Render 2 points -- prepare for vectorization

--- assembly/src/renderMandelbrot.ts
+++ assembly/src/renderMandelbrot.ts
@@ -41,53 +41,188 @@ export function renderMandelbrot(): void {
 
 function initializeBigNum() { /* +++ Generate initialization */ }
 
-const minSizeToSplit: u32 = 6;
+const minSizeToSplit: u32 = 16;
+
+const NULL_XY: u64 = 0xffff_ffff_ffff_ffff;
+const NOT_RENDERED_COLOR: u32 = 0;
+const NOT_RENDERED_TWO_COLOR: u64 = 0;
+
 
 function renderRect(pX0: u32, pY0: u32, pX1: u32, pY1: u32): void {
-  const pXMid: u32 = (pX0 + pX1) / 2;
-  const pYMid: u32 = (pY0 + pY1) / 2;
-
-  const p0 = renderPoint(pX0, pY0);
-  if (
-      // TODO depending on size, take more points
-      // Corners
-      p0 === renderPoint(pX0, pY1 - 1)
-      && p0 === renderPoint(pX1 - 1, pY0)
-      && p0 === renderPoint(pX1 - 1, pY1 - 1)
-
-      // Sides in the middle
-      && p0 === renderPoint(pX0, pYMid)
-      && p0 === renderPoint(pXMid, pY0)
-      && p0 === renderPoint(pX1 - 1, pYMid)
-      && p0 === renderPoint(pXMid, pY1 - 1)
-
-      && p0 === renderPoint(pXMid, pYMid)
-  ) {
+  const contourColor = getContourAndMidPointColor(pX0, pY0, pX1, pY1);
+  if (contourColor !== NOT_RENDERED_COLOR) {
     for (let pY: u32 = pY0; pY < pY1; pY++) {
       for (let pX: u32 = pX0; pX < pX1; pX++) {
-        store<u16>(2 * (pY * canvasW + pX), p0);
+        store<u16>(2 * (pY * canvasW + pX), contourColor as u16);
       }
     }
     return;
   }
 
+
   if (pX1 - pX0 >= minSizeToSplit) {
+    const pXMid: u32 = (pX0 + pX1) / 2;
     renderRect(pX0, pY0, pXMid, pY1);
     renderRect(pXMid, pY0, pX1, pY1);
     return;
   }
 
   if (pY1 - pY0 >= minSizeToSplit) {
+    const pYMid: u32 = (pY0 + pY1) / 2;
     renderRect(pX0, pY0, pX1, pYMid);
     renderRect(pX0, pYMid, pX1, pY1);
     return;
   }
 
+  let pendingXY: u64 = NULL_XY;
   for (let pY: u32 = pY0; pY < pY1; pY++) {
     for (let pX: u32 = pX0; pX < pX1; pX++) {
-      renderPoint(pX, pY);
+      const xy: u64 = (pX as u64) | ((pY as u64) << 32);
+      if (loadRendered(xy) === NOT_RENDERED_COLOR) {
+        if (pendingXY === NULL_XY) {
+          pendingXY = xy;
+        } else {
+          renderTwoPoints(pendingXY, xy);
+          pendingXY = NULL_XY;
+        }
+      }
     }
   }
+
+  if (pendingXY !== NULL_XY) {
+    const otherXY: u64 = (((pX1 + canvasW - 1) / 2) as u64) | ((((pY1 + canvasH - 1) / 2) as u64) << 32);
+    renderTwoPoints(pendingXY, otherXY);
+  }
+}
+
+/**
+ * @return True if the same color
+ */
+function getContourAndMidPointColor(x0: u32, y0: u32, x1: u32, y1: u32): u32 {
+  // return NOT_RENDERED_COLOR;
+  genState = 0;
+  genX0 = x0;
+  genY0 = y0;
+  genX1 = x1;
+  genY1 = y1;
+
+  let pendingXY: u64 = NULL_XY;
+  let prevTwoColors: u64 = NOT_RENDERED_TWO_COLOR;
+
+  for (let i: u32 = 0; ; i++) {
+    const xy = generateContourAndMidPoints();
+    if (xy === NULL_XY) {
+      // disregard pending XY
+      return prevTwoColors as u32;
+    }
+
+    const currOneColor: u32 = loadRendered(xy);
+    if (currOneColor === NOT_RENDERED_COLOR) {
+      if (pendingXY === NULL_XY) {
+        pendingXY = xy;
+      } else {
+        const currTwoColors = renderTwoPoints(pendingXY, xy);
+        if (prevTwoColors === NOT_RENDERED_TWO_COLOR) {
+          prevTwoColors = currTwoColors
+        } else if (prevTwoColors !== currTwoColors) {
+          return NOT_RENDERED_COLOR;
+        }
+        pendingXY = NULL_XY;
+      }
+    } else {
+      if (prevTwoColors === NOT_RENDERED_TWO_COLOR) {
+        prevTwoColors = currOneColor | (currOneColor << 32);
+      } else if ((prevTwoColors as u32) !== currOneColor) {
+        return NOT_RENDERED_COLOR;
+      }
+    }
+
+    if (i === 5 && (prevTwoColors as u32) !== maxIterations) {
+      // Quicker check for colored (diverging) rects
+      return prevTwoColors as u32;
+    }
+  }
+
+  // Unreachable in fact
+  // noinspection UnreachableCodeJS,JSUnusedAssignment
+  return prevTwoColors as u32;
+}
+
+const step: u64 = 10;
+let genState: u64 = 0;
+let genX0: u64 = 0;
+let genY0: u64 = 0;
+let genX1: u64 = 0;
+let genY1: u64 = 0;
+
+function generateContourAndMidPoints(): u64 {
+  const baseState = genState & 0xf;
+  let p = genState >>> 4;
+  switch (baseState as u32) {
+    case 0:
+      genState = 1;
+      return genX0 | (genY0 << 32);
+    case 1:
+      genState = 2;
+      return genX0 | ((genY1 - 1) << 32);
+    case 2:
+      genState = 3;
+      return (genX1 - 1) | (genY0 << 32);
+    case 3:
+      genState = 4;
+      return (genX1 - 1) | ((genY1 - 1) << 32);
+    case 4:
+      genState = 5 | ((genY0 + step) << 4);
+      return ((genX0 + genX1 - 1) / 2) | (((genY0 + genY1 - 1) / 2) << 32);
+    case 5:
+      // x0 (y0 -> y1)
+      if (p < genY1) {
+        genState = 5 | (p + step) << 4;
+        return genX0 | p << 32;
+      }
+      p = genX0 + step
+      // fall-through
+    case 6:
+      // (x0->x1) y1
+      if (p < genX1) {
+        genState = 6 | (p + step) << 4;
+        return p | (genY1 - 1) << 32;
+      }
+      p = genX0 + step;
+      // fall-through
+    case 7:
+      // (x0 -> x1) y0
+      if (p < genX1) {
+        genState = 7 | (p + step) << 4;
+        return p | genY0 << 32;
+      }
+      p = genY0 + step;
+      // fall-through
+    case 8:
+      // x1 (y0 -> y1)
+      if (p < genY1) {
+        genState = 8 | (p + step) << 4;
+        return (genX1 - 1) | p << 32;
+      }
+      // fall-through
+    default:
+      return NULL_XY;
+  }
+}
+
+
+function loadRendered(xy: u64): u32 {
+  const pY = (xy >>> 32) as u32;
+  const pX = xy as u32;
+  return load<u16>(2 * (pY * canvasW + pX)) as u32;
+}
+
+function renderTwoPoints(xy1: u64, xy2: u64): u64 {
+  // TODO vectorize
+  const c1 = renderPoint(xy1 as u32, (xy1 >>> 32) as u32) as u64;
+  const c2 = renderPoint(xy2 as u32, (xy2 >>> 32) as u32) as u64;
+
+  return c1 | (c2 << 32);
 }
 
 function renderPoint(pX: u32, pY: u32): u16 {
@@ -128,4 +263,5 @@ function renderPointDouble(pX: u32, pY: u32): u16 {
   return i as u16;
 }
 
+// noinspection JSUnusedLocalSymbols
 function renderPointBigNum(pX: u32, pY: u32): u16 { /* +++ Generate render */ return 0 }
